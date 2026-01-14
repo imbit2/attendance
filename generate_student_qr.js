685042c1c0d96dacff9069eed4dcb4e444927821
imbit2 <37894793+imbit2@users.noreply.github.com>
2026-01-14 13:36:03 +0530

    Update generate_student_qr.js

--- generate_student_qr.js
+++ generate_student_qr.js
@@ -1,12 +1,29 @@
 let students = JSON.parse(localStorage.getItem("students") || "[]");
 
+// generate next ID
 let next = students.length + 1;
 let sid = "STU" + String(next).padStart(4, "0");
+
+// fill ID
 id.value = sid;
 
-new QRCode(qr, sid);
+// show data on card
+cardId.innerText = sid;
 
-function save() {
+// generate QR
+qr.innerHTML = "";
+new QRCode(qr, {
+  text: sid,
+  width: 90,
+  height: 90
+});
+
+// live update card
+name.addEventListener("input", () => cardName.innerText = name.value);
+dob.addEventListener("change", () => cardDob.innerText = dob.value);
+
+// save student
+function saveStudent() {
   students.push({
     id: sid,
     name: name.value || "",
@@ -14,6 +31,12 @@ function save() {
     dob: dob.value || "",
     address: address.value || ""
   });
+
   localStorage.setItem("students", JSON.stringify(students));
-  alert("Saved");
+  alert("Student Saved Successfully");
+}
+
+// print pvc card
+function printCard() {
+  window.print();
 }
